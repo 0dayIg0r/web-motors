1d17ecd5d0a4e242e6eef073646610acc159e33c
0dayig0r <igormedeiros.f@gmail.com>
2023-07-15 18:08:09 -0300

    adicionando hook useForm e o zod para verificação

--- clone-web-motors/src/pages/login/index.tsx
+++ clone-web-motors/src/pages/login/index.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg'
+import { Container } from '../../components/container';
+import { Input } from '../../components/input';
+import {useForm} from 'react-hook-form'
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema =z.object({
+  email: z.string().email('Insira um e-mail válido').nonempty('O campo e-mail é obrigatório'),
+  passowrd: z.string().nonempty('Digite uma senha')
+})
+
+type FormData = z.infer<typeof schema>
+
 
 function Login() {
-  return <div>Login</div>;
+  const {register, handleSubmit, formState:{ errors }} = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: 'onChange'
+  })
+
+  function onSubmit(data: FormData){
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <div className=' w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+       <Link
+       className='mb-6 max-w-sm w-full' 
+       to='/'
+       >
+        <img 
+        className='w-full'
+        src={logoImg} 
+        alt="logo do site"
+         />
+       </Link>
+
+       <form 
+        className='bg-white max-w-xl rounded-lg w-full'
+        onSubmit={handleSubmit(onSubmit)}
+        >
+        <div className='mb-3'>
+        <Input
+        type='email'
+        placeholder= 'Digite seu e-mail' 
+        name='email'
+        error={errors.email?.message}
+        register={register}
+        />
+        </div>
+
+        <div className='mb-3'>
+        <Input
+        type='password'
+        placeholder= 'Digite sua senha' 
+        name='password'
+        error={errors.passowrd?.message}
+        register={register}
+        />
+        </div>
+
+        <button>
+          Acessar
+        </button>
+       </form>
+      </div>
+    </Container>
+  )
 }
 
 export { Login };
